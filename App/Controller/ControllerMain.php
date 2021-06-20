3222f64f4b98b5cf5c0a988bca1f6de1c6e163db
Vlad Butenko <vladbutenkoan@gmail.com>
2021-06-20 17:23:31 +0300

    Fixed markup and search module

--- App/Controller/ControllerMain.php
+++ App/Controller/ControllerMain.php
@@ -7,7 +7,7 @@ class ControllerMain extends \Framework\Core\Controller
 {
     public function actionIndex()
     {
-        $product_list = ProductMapper::getProducts(5);
+        $product_list = ProductMapper::getProducts(8);
         $this->renderer->render("layout_view", $product_list, 'main_view');
     }
 }
